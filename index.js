4f768bd824d305c5b470e46c09b3404028ac7d47
Khuzama Shahid <skhuzamashahid@gmail.com>
2020-03-03 18:37:09 +0500

    added setting for ssl

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let express = require("express"),
     app = express(),
     cors = require('cors')
-var http = require('http');
+    var https = require('https');
+    var http = require('http');
+    var fs = require('fs');
 app.use(cors())
 app.use('/', express.static(__dirname + '/build')) //project k root m build k name se folder banaiega or wahan react ki build daal dijiega
+
+var options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/xio.app/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/xio.app/fullchain.pem')
+  };
+  
 http.createServer(app).listen(3000);
+https.createServer(options, app).listen(443);
